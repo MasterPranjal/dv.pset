085f45a0b96f9376c9f8a63c26dfff587171bd91
MasterPranjal <pranjal_jain@live.com>
2018-05-26 13:53:16 +0530

    temperature conversion

---
+++ 0041/temperature.py
@@ -0,0 +1,18 @@
+c = int(input("Enter Celsius value:"))
+f = int(input("Enter Fahrenheit value:"))
+def celsius(f):
+    '''
+    Returns the Celsius equivalent of Fahrenheit input
+    '''
+    f = (9/5)*Celsius+32
+    return f
+
+def fahrenheit(c):
+    '''
+    Returns the Fahrenheit equivalent of Celsius input
+    '''
+    C = (5*Fahrenheit/9 - 32)
+    return c
+
+
+
