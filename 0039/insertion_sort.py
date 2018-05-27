a37f15eeafc0ded11fe18b203115d3d5e8d51e8e
MasterPranjal <pranjal_jain@live.com>
2018-05-27 23:30:38 +0530

    insertion sort

---
+++ 0039/insertion_sort.py
@@ -0,0 +1,14 @@
+print("enter a number of items with space")
+items=input().split(' ')
+M=[int(i) for i in items]
+
+def insertion_sort(L):
+    for j in range(5):
+        for i in range(5):
+            if(L[i]>L[i+1]):
+                k=L[i+1]
+                L[i+1]=L[i]
+                L[i]=k
+    return L
+
+print(insertion_sort(M))
