3e3f907639c32a57268d3a7af7054a65d6118b2b
MasterPranjal <pranjal_jain@live.com>
2018-05-31 23:30:11 +0530

    addition

--- 0057/consecutive_number.py
+++ 0057/consecutive_number.py
@@ -5,3 +5,17 @@ for i in range(0,x,1):
         print(n, end=" ")
         n = n + 1
 print()
+
+
+##another approach
+def conse_num(n):
+     
+    num = 1
+ 
+    for i in range(0, n):
+        for j in range(0, i+1)
+        
+            print(num, end=" ")
+            num = num + 1
+     
+        print("\r") 
