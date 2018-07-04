e3db30aa309254d1bf9c1dfe62de259bdbe26564
MasterPranjal <pranjal_jain@live.com>
2018-07-04 23:58:37 +0530

    sign function

--- 0075/SignFunction.py
+++ 0075/SignFunction.py
@@ -1,11 +1,11 @@
 #Write sign function: returns +1 or -1 based on the sign of argument passed.
 
-number = int(input('Enter a number to check its sign is it (+ or -) = '))
+num = int(input('Enter a number to check its sign is it (+ or -) = '))
 
-def signFunction(number):
-    if number < 0:
+def signFunction(num):
+    if num < 0:
         print ("-1")
     else:
         print ("+1")
 
-signFunction()
+signFunction(num)
