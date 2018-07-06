5be11f915e757b8cb62f651adc5b81c1646c99e0
MasterPranjal <pranjal_jain@live.com>
2018-07-06 23:59:31 +0530

    Indicator function

---
+++ 0074/Indicator_function.py
@@ -0,0 +1,15 @@
+# Write Indicator function: maps x to either 1 or 0, depending on
+# whether or not x belongs to given set.
+
+A = [2, 5, 6, 8, 9, 10]
+
+x = int(input("Enter the number which belongs or does not belongs = "))
+
+def indicator(A,x):
+    while ()
+        if (x == A[j]):
+            print("x = 1")
+        else:
+            print("x = 0")
+
+indicator(A,x)
