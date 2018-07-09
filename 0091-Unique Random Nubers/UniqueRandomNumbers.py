052a011813559e3040d7a4de49e0880c27985628
MasterPranjal <pranjal_jain@live.com>
2018-07-10 00:00:41 +0530

    Unique Random Numbers

---
+++ 0091-Unique Random Nubers/UniqueRandomNumbers.py
@@ -0,0 +1,12 @@
+#Write a program that generates 5 unique random numbers.
+
+import random
+a=int(random.random()*10)
+b=int(random.random()*10)
+c=int(random.random()*10)
+
+def generateRandon(a,b,c):
+    while (i!=j!=k):
+        print (i,j,k)
+    break
+
